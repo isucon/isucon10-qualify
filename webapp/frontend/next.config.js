3c4a5eff8f5cc67dbd267c8cabcfe10feccf565b
Shunsuke Mano <19568747+progfay@users.noreply.github.com>
2020-06-05 18:35:28 +0900

    build時にIP等を環境変数から読み込む (#46)
    * npm run deployでnginxが静的ファイル配信をできる位置に移動する
    
    * build時にIP等を環境変数から読み込む

--- webapp/frontend/next.config.js
+++ webapp/frontend/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     API_SERVER_NAME: process.env.NODE_ENV === 'production'
-      ? `http://${process.env.API_SERVER_HOST}:${process.env.API_SERVER_PORT}`
+      ? `http://${process.env.API_SERVER_HOST || 'localhost'}:${process.env.API_SERVER_PORT || 3010}`
       : 'http://localhost:3010'
   },
 }
