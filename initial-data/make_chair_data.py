43a3c0f13cd2d79466c9db89577fae735d9e7142
Shunsuke Mano <19568747+progfay@users.noreply.github.com>
2020-06-10 10:05:28 +0900

    ダミー画像の生成枚数を制限する (#66)

--- initial-data/make_chair_data.py
+++ initial-data/make_chair_data.py
@@ -4,6 +4,7 @@ import time
 import string
 import json
 import os
+import glob
 from faker import Faker
 fake = Faker("ja_JP")
 Faker.seed(19700101)
@@ -14,6 +15,7 @@ OUTPUT_SQL_FILE = "./result/2_DummyChairData.sql"
 OUTPUT_TXT_FILE = "./result/chair_json.txt"
 CHAIR_IMAGE_ORIGIN_DIR = "./origin/chair"
 CHAIR_IMAGE_PUBLIC_DIR = "../webapp/frontend/public/images/chair"
+CHAIR_DUMMY_IMAGE_NUM = 1000
 RECORD_COUNT = 10 ** 4
 BULK_INSERT_COUNT = 500
 CHAIR_MIN_CENTIMETER = 30
@@ -66,7 +68,7 @@ CHAIR_PROPERTY_LIST = [
     "アウトドア",
 ]
 
-CHAIR_NAME_LIST= [
+CHAIR_NAME_LIST = [
     "イス",
     "チェア",
     "フロアチェア",
@@ -96,31 +98,22 @@ CHAIR_KIND_LIST = [
     "ハンモック"
 ]
 
-CHAIR_IMAGE_LIST = [
-    os.path.join(CHAIR_IMAGE_ORIGIN_DIR, "1501E5C34A2B8EE645480ED1CC6442CD5929FE7616E20513574628096163DF0C.jpg"),
-    os.path.join(CHAIR_IMAGE_ORIGIN_DIR, "3E880A828B1DBFACB42209724583B56EF28466E45E2BF3704475EA02B19BDBFC.jpg"),
-    os.path.join(CHAIR_IMAGE_ORIGIN_DIR, "9120C2E3CAF5CD376C1B14899C2FD31438A839D1F6B6F8A52091392E0B9168FC.jpg")
-]
+CHAIR_IMAGE_HASH_LIST = [fake.sha256(
+    raw_output=False) for _ in range(CHAIR_DUMMY_IMAGE_NUM)]
+
 
 def read_src_file_data(file_path):
     with open(file_path, mode='rb') as img:
         return img.read()
 
-CHAIR_IMAGE_DATA = [read_src_file_data(image) for image in CHAIR_IMAGE_LIST]
 
 def generate_chair_dummy_data(chair_id):
     features_length = random.randint(0, len(CHAIR_FEATURE_LIST) - 1)
-
-    new_chair_image_hash = fake.sha256(raw_output=False)
-    new_chair_image_path = os.path.join(CHAIR_IMAGE_PUBLIC_DIR, "{}.jpg".format(new_chair_image_hash))
-    src_image_data = random.choice(CHAIR_IMAGE_DATA)
-
-    with open(new_chair_image_path, mode='wb') as dst_image_data:
-        dst_image_data.write(src_image_data + new_chair_image_hash.encode('utf-8'))
+    image_hash = fake.word(ext_word_list=CHAIR_IMAGE_HASH_LIST)
 
     return {
         "id": chair_id,
-        "thumbnail": "/images/chair/{}.jpg".format(new_chair_image_hash),
+        "thumbnail": f'/images/chair/{image_hash}.png',
         "name": "".join([
             fake.word(ext_word_list=CHAIR_NAME_PREFIX_LIST),
             fake.word(ext_word_list=CHAIR_PROPERTY_LIST),
@@ -138,11 +131,14 @@ def generate_chair_dummy_data(chair_id):
         "kind": fake.word(ext_word_list=CHAIR_KIND_LIST)
     }
 
+
 if __name__ == "__main__":
-    for image in CHAIR_IMAGE_LIST:
-        filename, _ = os.path.splitext(os.path.basename(image))
-        with open(os.path.join(CHAIR_IMAGE_PUBLIC_DIR, "{}.jpg".format(filename)), mode='wb') as dst_image_data:
-            dst_image_data.write(read_src_file_data(image) + filename.encode('utf-8'))
+    for i, random_hash in enumerate(CHAIR_IMAGE_HASH_LIST):
+        image_data_list = [read_src_file_data(
+            image) for image in glob.glob(os.path.join(CHAIR_IMAGE_ORIGIN_DIR, "*.png"))]
+        with open(os.path.join(CHAIR_IMAGE_PUBLIC_DIR, f"{random_hash}.png"), mode='wb') as image_file:
+            image_file.write(
+                image_data_list[i % len(image_data_list)] + random_hash.encode('utf-8'))
 
     with open(DESCRIPTION_LINES_FILE, mode="r") as description_lines:
         desc_lines = description_lines.readlines()
@@ -150,11 +146,13 @@ if __name__ == "__main__":
     with open(OUTPUT_SQL_FILE, mode='w') as sqlfile, open(OUTPUT_TXT_FILE, mode='w') as txtfile:
         sqlfile.write(sqlCommands)
         if RECORD_COUNT % BULK_INSERT_COUNT != 0:
-            raise Exception("The results of RECORD_COUNT and BULK_INSERT_COUNT need to be a divisible number. RECORD_COUNT = {}, BULK_INSERT_COUNT = {}".format(RECORD_COUNT, BULK_INSERT_COUNT))
+            raise Exception("The results of RECORD_COUNT and BULK_INSERT_COUNT need to be a divisible number. RECORD_COUNT = {}, BULK_INSERT_COUNT = {}".format(
+                RECORD_COUNT, BULK_INSERT_COUNT))
 
         chair_id = 1
         for _ in range(RECORD_COUNT // BULK_INSERT_COUNT):
-            bulk_list = [generate_chair_dummy_data(chair_id + i) for i in range(BULK_INSERT_COUNT)]
+            bulk_list = [generate_chair_dummy_data(
+                chair_id + i) for i in range(BULK_INSERT_COUNT)]
             chair_id += BULK_INSERT_COUNT
             sqlCommand = f"""INSERT INTO chair (id, thumbnail, name, price, height, width, depth, view_count, stock, color, description, features, kind) VALUES {", ".join(map(lambda chair: f"('{chair['id']}', '{chair['thumbnail']}', '{chair['name']}', '{chair['price']}', '{chair['height']}', '{chair['width']}', '{chair['depth']}', '{chair['view_count']}', '{chair['stock']}', '{chair['color']}', '{chair['description']}', '{chair['features']}', '{chair['kind']}')", bulk_list))};"""
             sqlfile.write(sqlCommand)
--- initial-data/make_estate_data.py
+++ initial-data/make_estate_data.py
@@ -4,6 +4,7 @@ import time
 import string
 import json
 import os
+import glob
 from faker import Faker
 fake = Faker('ja_JP')
 Faker.seed(19700101)
@@ -14,6 +15,7 @@ OUTPUT_SQL_FILE = "./result/1_DummyEstateData.sql"
 OUTPUT_TXT_FILE = "./result/estate_json.txt"
 ESTATE_IMAGE_ORIGIN_DIR = "./origin/estate"
 ESTATE_IMAGE_PUBLIC_DIR = "../webapp/frontend/public/images/estate"
+ESTATE_DUMMY_IMAGE_NUM = 1000
 RECORD_COUNT = 10 ** 4
 BULK_INSERT_COUNT = 500
 DOOR_MIN_CENTIMETER = 30
@@ -37,34 +39,23 @@ ESTATE_FEATURE_LIST = [
     "デザイナーズ物件",
 ]
 
-ESTATE_IMAGE_LIST = [
-    os.path.join(ESTATE_IMAGE_ORIGIN_DIR, "1501E5C34A2B8EE645480ED1CC6442CD5929FE7616E20513574628096163DF0C.jpg"),
-    os.path.join(ESTATE_IMAGE_ORIGIN_DIR, "3E880A828B1DBFACB42209724583B56EF28466E45E2BF3704475EA02B19BDBFC.jpg"),
-    os.path.join(ESTATE_IMAGE_ORIGIN_DIR, "9120C2E3CAF5CD376C1B14899C2FD31438A839D1F6B6F8A52091392E0B9168FC.jpg")
-]
+ESTATE_IMAGE_HASH_LIST = [fake.sha256(
+    raw_output=False) for _ in range(ESTATE_DUMMY_IMAGE_NUM)]
+
 
 def read_src_file_data(file_path):
     with open(file_path, mode='rb') as img:
         return img.read()
 
-ESTATE_IMAGE_DATA = [read_src_file_data(image) for image in ESTATE_IMAGE_LIST]
 
 def generate_estate_dummy_data(estate_id):
     latlng = fake.local_latlng(country_code='JP', coords_only=True)
     feature_length = random.randint(0, len(ESTATE_FEATURE_LIST) - 1)
-
-    new_estate_image_hash = fake.sha256(raw_output=False)
-    new_estate_image_path = os.path.join(ESTATE_IMAGE_PUBLIC_DIR, "{}.jpg".format(new_estate_image_hash))
-    src_image_data = random.choice(ESTATE_IMAGE_DATA)
-
-    with open(new_estate_image_path, mode='wb') as dst_image_data:
-        dst_image_data.write(src_image_data + new_estate_image_hash.encode('utf-8'))
-
-    src_estate_image_filename = fake.word(ext_word_list=ESTATE_IMAGE_LIST)
+    image_hash = fake.word(ext_word_list=ESTATE_IMAGE_HASH_LIST)
 
     return {
         "id": estate_id,
-        "thumbnail": '/images/estate/{}.jpg'.format(new_estate_image_hash),
+        "thumbnail": f'/images/estate/{image_hash}.png',
         "name": fake.word(ext_word_list=BUILDING_NAME_LIST).format(name=fake.last_name()),
         "latitude": float(latlng[0]),
         "longitude": float(latlng[1]),
@@ -77,11 +68,14 @@ def generate_estate_dummy_data(estate_id):
         "features": ','.join(fake.words(nb=feature_length, ext_word_list=ESTATE_FEATURE_LIST, unique=True))
     }
 
+
 if __name__ == '__main__':
-    for image in ESTATE_IMAGE_LIST:
-        filename, _ = os.path.splitext(os.path.basename(image))
-        with open(os.path.join(ESTATE_IMAGE_PUBLIC_DIR, "{}.jpg".format(filename)), mode='wb') as dst_image_data:
-            dst_image_data.write(read_src_file_data(image) + filename.encode('utf-8'))
+    for i, random_hash in enumerate(ESTATE_IMAGE_HASH_LIST):
+        image_data_list = [read_src_file_data(
+            image) for image in glob.glob(os.path.join(ESTATE_IMAGE_ORIGIN_DIR, "*.png"))]
+        with open(os.path.join(ESTATE_IMAGE_PUBLIC_DIR, f"{random_hash}.png"), mode='wb') as image_file:
+            image_file.write(
+                image_data_list[i % len(image_data_list)] + random_hash.encode('utf-8'))
 
     with open(DESCRIPTION_LINES_FILE, mode='r') as description_lines:
         desc_lines = description_lines.readlines()
@@ -89,11 +83,13 @@ if __name__ == '__main__':
     with open(OUTPUT_SQL_FILE, mode='w') as sqlfile, open(OUTPUT_TXT_FILE, mode='w') as txtfile:
         sqlfile.write(sqlCommands)
         if RECORD_COUNT % BULK_INSERT_COUNT != 0:
-            raise Exception("The results of RECORD_COUNT and BULK_INSERT_COUNT need to be a divisible number. RECORD_COUNT = {}, BULK_INSERT_COUNT = {}".format(RECORD_COUNT, BULK_INSERT_COUNT))
+            raise Exception("The results of RECORD_COUNT and BULK_INSERT_COUNT need to be a divisible number. RECORD_COUNT = {}, BULK_INSERT_COUNT = {}".format(
+                RECORD_COUNT, BULK_INSERT_COUNT))
 
         estate_id = 1
         for _ in range(RECORD_COUNT//BULK_INSERT_COUNT):
-            bulk_list = [generate_estate_dummy_data(estate_id + i) for i in range(BULK_INSERT_COUNT)]
+            bulk_list = [generate_estate_dummy_data(
+                estate_id + i) for i in range(BULK_INSERT_COUNT)]
             estate_id += BULK_INSERT_COUNT
             sqlCommand = f"""INSERT INTO estate (id, thumbnail, name, latitude, longitude, address, rent, door_height, door_width, view_count, description, features) VALUES {', '.join(map(lambda estate: f"('{estate['id']}', '{estate['thumbnail']}', '{estate['name']}', '{estate['latitude']}' , '{estate['longitude']}', '{estate['address']}', '{estate['rent']}', '{estate['door_height']}', '{estate['door_width']}', '{estate['view_count']}', '{estate['description']}', '{estate['features']}')", bulk_list))};"""
             sqlfile.write(sqlCommand)
