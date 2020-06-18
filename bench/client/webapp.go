7c1bc4b85044d6be0ff647d8d5b94763ac76d3e0
Shunsuke Mano <19568747+progfay@users.noreply.github.com>
2020-06-18 11:31:56 +0900

    ベンチマーカーからのリクエストにそれぞれタイムアウトを設定する (#98)
    Co-authored-by: mikiya771 <01036173@CA6100.local>
    Co-authored-by: mikiya771 <ku7wa7no1mikiya@gmail.com>

--- bench/client/webapp.go
+++ bench/client/webapp.go
@@ -9,10 +9,23 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"time"
 
 	"github.com/isucon10-qualify/isucon10-qualify/bench/asset"
+	"github.com/isucon10-qualify/isucon10-qualify/bench/fails"
 	"github.com/isucon10-qualify/isucon10-qualify/bench/passes"
 	"github.com/morikuni/failure"
+)
+
+const (
+	timeoutOfGetChairDetailFromID           = 300 * time.Millisecond
+	timeoutOfSearchChairsWithQuery          = 500 * time.Millisecond
+	timeoutOfGetEstateDetailFromID          = 300 * time.Millisecond
+	timeoutOfSearchEstatesWithQuery         = 500 * time.Millisecond
+	timeoutOfSearchEstatesNazotte           = 800 * time.Millisecond
+	timeoutOfGetRecommendedEstatesFromChair = 300 * time.Millisecond
+	timeoutOfBuyChair                       = 300 * time.Millisecond
+	timeoutOfRequestEstateDocument          = 300 * time.Millisecond
 )
 
 type Coordinates struct {
@@ -64,25 +77,32 @@ func (c *Client) GetChairDetailFromID(ctx context.Context, id string) (*asset.Ch
 		return nil, failure.Wrap(err, failure.Message("GET /api/chair/:id: リクエストに失敗しました"))
 	}
 
-	req = req.WithContext(ctx)
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeoutOfGetChairDetailFromID)
+	defer cancel()
 
+	req = req.WithContext(timeoutCtx)
 	res, err := c.Do(req)
+
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
 		}
-		return nil, failure.Wrap(err, failure.Message("GET /api/chair/:id: リクエストに失敗しました"))
-	}
-	if res == nil {
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/chair/:id: リクエストがタイムアウトしました"))
+		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/chair/:id: リクエストに失敗しました"))
 	}
 	defer res.Body.Close()
 
 	var chair asset.Chair
+
 	err = json.NewDecoder(res.Body).Decode(&chair)
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
+		}
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/chair/:id: リクエストがタイムアウトしました"))
 		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/chair/:id: JSONデコードに失敗しました"))
 	}
@@ -99,16 +119,19 @@ func (c *Client) SearchChairsWithQuery(ctx context.Context, q url.Values) (*Chai
 		return nil, failure.Wrap(err, failure.Message("GET /api/chair/search: リクエストに失敗しました"))
 	}
 
-	req = req.WithContext(ctx)
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeoutOfSearchChairsWithQuery)
+	defer cancel()
 
+	req = req.WithContext(timeoutCtx)
 	res, err := c.Do(req)
+
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
 		}
-		return nil, failure.Wrap(err, failure.Message("GET /api/chair/search: リクエストに失敗しました"))
-	}
-	if res == nil {
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/chair/search: リクエストがタイムアウトしました"))
+		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/chair/search: リクエストに失敗しました"))
 	}
 	defer res.Body.Close()
@@ -119,6 +142,9 @@ func (c *Client) SearchChairsWithQuery(ctx context.Context, q url.Values) (*Chai
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
+		}
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/chair/search: リクエストがタイムアウトしました"))
 		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/chair/search: JSONデコードに失敗しました"))
 	}
@@ -135,16 +161,19 @@ func (c *Client) SearchEstatesWithQuery(ctx context.Context, q url.Values) (*Est
 		return nil, failure.Wrap(err, failure.Message("GET /api/estate/search: リクエストに失敗しました"))
 	}
 
-	req = req.WithContext(ctx)
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeoutOfSearchEstatesWithQuery)
+	defer cancel()
 
+	req = req.WithContext(timeoutCtx)
 	res, err := c.Do(req)
+
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
 		}
-		return nil, failure.Wrap(err, failure.Message("GET /api/estate/search: リクエストに失敗しました"))
-	}
-	if res == nil {
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/estate/search: リクエストがタイムアウトしました"))
+		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/estate/search: リクエストに失敗しました"))
 	}
 	defer res.Body.Close()
@@ -155,6 +184,9 @@ func (c *Client) SearchEstatesWithQuery(ctx context.Context, q url.Values) (*Est
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
+		}
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/estate/search: リクエストがタイムアウトしました"))
 		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/estate/search: JSONデコードに失敗しました"))
 	}
@@ -175,22 +207,32 @@ func (c *Client) SearchEstatesNazotte(ctx context.Context, polygon *Coordinates)
 		return nil, failure.Wrap(err, failure.Message("POST /api/estate/nazotte: リクエストに失敗しました"))
 	}
 
-	req = req.WithContext(ctx)
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeoutOfSearchEstatesNazotte)
+	defer cancel()
 
+	req = req.WithContext(timeoutCtx)
 	res, err := c.Do(req)
+
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
+		}
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/estate/search: リクエストがタイムアウトしました"))
 		}
 		return nil, failure.Wrap(err, failure.Message("POST /api/estate/nazotte: リクエストに失敗しました"))
 	}
 	defer res.Body.Close()
 
 	var estates EstatesResponse
+
 	err = json.NewDecoder(res.Body).Decode(&estates)
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
+		}
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/estate/search: リクエストがタイムアウトしました"))
 		}
 		return nil, failure.Wrap(err, failure.Message("POST /api/estate/nazotte: JSONデコードに失敗しました"))
 	}
@@ -203,16 +245,19 @@ func (c *Client) GetEstateDetailFromID(ctx context.Context, id string) (*asset.E
 		return nil, failure.Wrap(err, failure.Message("GET /api/estate/:id: リクエストに失敗しました"))
 	}
 
-	req = req.WithContext(ctx)
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeoutOfGetEstateDetailFromID)
+	defer cancel()
 
+	req = req.WithContext(timeoutCtx)
 	res, err := c.Do(req)
+
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
 		}
-		return nil, failure.Wrap(err, failure.Message("GET /api/estate/:id: リクエストに失敗しました"))
-	}
-	if res == nil {
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/estate/:id: リクエストがタイムアウトしました"))
+		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/estate/:id: リクエストに失敗しました"))
 	}
 	defer res.Body.Close()
@@ -223,6 +268,9 @@ func (c *Client) GetEstateDetailFromID(ctx context.Context, id string) (*asset.E
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
+		}
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/estate/:id: リクエストがタイムアウトしました"))
 		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/estate/:id: JSONデコードに失敗しました"))
 	}
@@ -239,25 +287,32 @@ func (c *Client) GetRecommendedEstatesFromChair(ctx context.Context, id int64) (
 		return nil, failure.Wrap(err, failure.Message("GET /api/recommended_estate/:id: リクエストに失敗しました"))
 	}
 
-	req = req.WithContext(ctx)
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeoutOfGetRecommendedEstatesFromChair)
+	defer cancel()
 
+	req = req.WithContext(timeoutCtx)
 	res, err := c.Do(req)
+
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
 		}
-		return nil, failure.Wrap(err, failure.Message("GET /api/recommended_estate/:id: リクエストに失敗しました"))
-	}
-	if res == nil {
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/recommended_estate/:id: リクエストがタイムアウトしました"))
+		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/recommended_estate/:id: リクエストに失敗しました"))
 	}
 	defer res.Body.Close()
 
 	var estate EstatesResponse
+
 	err = json.NewDecoder(res.Body).Decode(&estate)
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return nil, ctxErr
+		}
+		if timeoutCtx.Err() != nil {
+			return nil, failure.New(fails.ErrTimeout, failure.Message("GET /api/recommended_estate/:id: リクエストがタイムアウトしました"))
 		}
 		return nil, failure.Wrap(err, failure.Message("GET /api/recommended_estate/:id: JSONデコードに失敗しました"))
 	}
@@ -273,15 +328,22 @@ func (c *Client) BuyChair(ctx context.Context, id string) error {
 		return failure.Wrap(err, failure.Message("POST /api/chair/buy/:id: リクエストに失敗しました"))
 	}
 
-	req = req.WithContext(ctx)
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeoutOfBuyChair)
+	defer cancel()
 
+	req = req.WithContext(timeoutCtx)
 	res, err := c.Do(req)
+
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return ctxErr
 		}
+		if timeoutCtx.Err() != nil {
+			return failure.New(fails.ErrTimeout, failure.Message("POST /api/chair/buy/:id: リクエストがタイムアウトしました"))
+		}
 		return failure.Wrap(err, failure.Message("POST /api/chair/buy/:id: リクエストに失敗しました"))
 	}
+	defer res.Body.Close()
 
 	err = checkStatusCode(res, 200)
 	if err != nil {
@@ -301,15 +363,22 @@ func (c *Client) RequestEstateDocument(ctx context.Context, id string) error {
 		return failure.Wrap(err, failure.Message("POST /api/estate/req_doc/:id: リクエストに失敗しました"))
 	}
 
-	req = req.WithContext(ctx)
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeoutOfRequestEstateDocument)
+	defer cancel()
 
+	req = req.WithContext(timeoutCtx)
 	res, err := c.Do(req)
+
 	if err != nil {
 		if ctxErr := ctx.Err(); ctxErr != nil {
 			return ctxErr
 		}
+		if timeoutCtx.Err() != nil {
+			return failure.New(fails.ErrTimeout, failure.Message("POST /api/estate/req_doc/:id: リクエストがタイムアウトしました"))
+		}
 		return failure.Wrap(err, failure.Message("POST /api/estate/req_doc/:id: リクエストに失敗しました"))
 	}
+	defer res.Body.Close()
 
 	err = checkStatusCode(res, 200)
 	if err != nil {
