2c8b54f1a8ce24039302d9b0ed4a6c7b7f0acd95
mikiya771 <ku7wa7no1mikiya@gmail.com>
2020-06-23 15:15:17 +0900

    [initial-data][benchmarker]初期状態におけるリクエストレスポンスの対応関係の自動生成 (#136)
    * fix: estate/nazotte add Limitation
    
    * add: golang経由でapi-serverを叩くことで初期状態のリクエストとレスポンスの対応関係を得られるようにした
    
    * gitignoreに生成したリクエストレスポンス対応ファイルを足した
    
    * chair/searchの初期データもとってこれるようにした
    
    * fix: .gitignore
    
    * fix: add verifiaction to APIs
    
    * feat: Makefile/make verification data
    
    * add: recommend_estates_with_chair requests
    
    * fix: auto make verifiaction-data
    
    * add : template jsons
    
    * fix: request json names
    
    * change: recommend-estate/idのリクエストの仕様の変更
    
    * change: recommend-estate/idのリクエストの仕様の変更
    
    * change: recommend-estate/idのリクエストの仕様の変更
    
    Co-authored-by: mikiya771 <01036173@CA6100.local>

---
+++ initial-data/types.go
@@ -0,0 +1,82 @@
+package main
+
+type RequestFrame struct {
+	RequectContent Request `json:"request"`
+}
+type Request struct {
+	Method string      `json:"method"`
+	Uri    string      `json:"uri"`
+    Id string `json:"id"`
+	Query  Queries     `json:"query"`
+	Body   Coordinates `json:"body"`
+}
+type Coordinates struct {
+	Coordinate []Coordinate `json:"coordinates"`
+}
+type Coordinate struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+type EstatesAnswerJson struct {
+	Req Request     `json:"request"`
+	Res EstatesBody `json:"response"`
+}
+type EstatesBody struct {
+	Body EstateResponse `json:"body"`
+}
+type ChairsAnswerJson struct {
+	Req Request    `json:"request"`
+	Res ChairsBody `json:"response"`
+}
+type ChairsBody struct {
+	Body ChairsResponse `json:"body"`
+}
+type EstateResponse struct {
+	Count   string   `json:"count"`
+	Estates []Estate `json:"estates"`
+}
+type ChairsResponse struct {
+	Count  string  `json:"count"`
+	Chairs []Chair `json:"chairs"`
+}
+type Estate struct {
+	ID          int64   `json:"id"`
+	Thumbnail   string  `json:"thumbnail"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Address     string  `json:"address"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+	DoorHeight  int64   `json:"doorHeight"`
+	DoorWidth   int64   `json:"doorWidth"`
+	Rent        int64   `json:"rent"`
+	Features    string  `json:"features"`
+}
+type Chair struct {
+	ID          int64  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Thumbnail   string `json:"thumbnail"`
+	Price       int64  `json:"price"`
+	Height      int64  `json:"height"`
+	Width       int64  `json:"width"`
+	Depth       int64  `json:"depth"`
+	Color       string `json:"color"`
+	Features    string `json:"features"`
+	Kind        string `json:"kind"`
+}
+
+type Queries struct {
+	RentRangeId       string `json:"rentRangeId"`
+	PriceRangeId      string `json:"priceRangeId"`
+	DoorHeightRangeId string `json:"doorHeightRangeId"`
+	DoorWidthRangeId  string `json:"doorWidthRangeId"`
+	HeightRangeId     string `json:"heightRangeId"`
+	WidthRangeId      string `json:"widthRangeId"`
+	DepthRangeId      string `json:"depthRangeId"`
+	Features          string `json:"features"`
+	Kind              string `json:"kind"`
+	Color             string `json:"color"`
+	Page              string `json:"page"`
+	PerPage           string `json:"perPage"`
+}
