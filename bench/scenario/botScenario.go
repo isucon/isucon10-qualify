d83b37ee327bc4df383cd5e861a10594c92b2efe
progfay <shun1856shun@gmail.com>
2020-07-14 15:10:56 +0900

    botは検索後に10件詳細を取得しに行く

--- bench/scenario/botScenario.go
+++ bench/scenario/botScenario.go
@@ -2,6 +2,7 @@ package scenario
 
 import (
 	"context"
+	"strconv"
 	"sync"
 
 	"github.com/isucon10-qualify/isucon10-qualify/bench/client"
@@ -17,13 +18,25 @@ func botScenario(ctx context.Context, c *client.Client) {
 		defer wg.Done()
 		q := generateRandomQueryForSearchChairs()
 		q.Set("perPage", "10")
-		_, err := c.SearchChairsWithQuery(ctx, q)
+		chairs, err := c.SearchChairsWithQuery(ctx, q)
 		if err != nil {
 			code, _ := failure.CodeOf(err)
 			if code != fails.ErrBot {
 				fails.ErrorsForCheck.Add(err, fails.ErrorOfBotScenario)
 			}
 			return
+		}
+
+		for _, chair := range chairs.Chairs {
+			wg.Add(1)
+			go func(id string) {
+				defer wg.Done()
+				_, err := c.GetChairDetailFromID(ctx, id)
+				code, _ := failure.CodeOf(err)
+				if code != fails.ErrBot {
+					fails.ErrorsForCheck.Add(err, fails.ErrorOfBotScenario)
+				}
+			}(strconv.FormatInt(chair.ID, 10))
 		}
 	}()
 
@@ -32,13 +45,25 @@ func botScenario(ctx context.Context, c *client.Client) {
 		defer wg.Done()
 		q := generateRandomQueryForSearchEstates()
 		q.Set("perPage", "10")
-		_, err := c.SearchEstatesWithQuery(ctx, q)
+		estates, err := c.SearchEstatesWithQuery(ctx, q)
 		if err != nil {
 			code, _ := failure.CodeOf(err)
 			if code != fails.ErrBot {
 				fails.ErrorsForCheck.Add(err, fails.ErrorOfBotScenario)
 			}
 			return
+		}
+
+		for _, estate := range estates.Estates {
+			wg.Add(1)
+			go func(id string) {
+				defer wg.Done()
+				_, err := c.GetEstateDetailFromID(ctx, id)
+				code, _ := failure.CodeOf(err)
+				if code != fails.ErrBot {
+					fails.ErrorsForCheck.Add(err, fails.ErrorOfBotScenario)
+				}
+			}(strconv.FormatInt(estate.ID, 10))
 		}
 	}()
 
