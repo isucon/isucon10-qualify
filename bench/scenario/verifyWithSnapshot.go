b0e50a133a5f868fc2f39816e6166b8092efb7dc
Shunsuke Mano <19568747+progfay@users.noreply.github.com>
2020-07-30 16:03:03 +0900

    [bench] snapshot形式のVerifyが落ちる理由をlogとして出力する (#26)

--- bench/scenario/verifyWithSnapshot.go
+++ bench/scenario/verifyWithSnapshot.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
@@ -93,8 +94,8 @@ func verifyChairSearch(ctx context.Context, c *client.Client, filePath string) e
 		}
 
 		if !cmp.Equal(*expected, *actual, ignoreChairUnexported) {
-			diff := cmp.Diff(*expected, *actual, ignoreChairUnexported)
-			return failure.New(fails.ErrApplication, failure.Message("GET /api/chair/search: イスの検索結果が不正です"), failure.Messagef("snapshot: %s", filePath), failure.Message(diff))
+			log.Printf("%s\n%s\n", filePath, cmp.Diff(*expected, *actual, ignoreChairUnexported))
+			return failure.New(fails.ErrApplication, failure.Message("GET /api/chair/search: イスの検索結果が不正です"), failure.Messagef("snapshot: %s", filePath))
 		}
 
 	default:
@@ -132,8 +133,8 @@ func verifyEstateSearch(ctx context.Context, c *client.Client, filePath string) 
 		}
 
 		if !cmp.Equal(*expected, *actual, ignoreEstateUnexported) {
-			diff := cmp.Diff(*expected, *actual, ignoreEstateUnexported)
-			return failure.New(fails.ErrApplication, failure.Message("GET /api/estate/search: 物件の検索結果が不正です"), failure.Messagef("snapshot: %s", filePath), failure.Message(diff))
+			log.Printf("%s\n%s\n", filePath, cmp.Diff(*expected, *actual, ignoreEstateUnexported))
+			return failure.New(fails.ErrApplication, failure.Message("GET /api/estate/search: 物件の検索結果が不正です"), failure.Messagef("snapshot: %s", filePath))
 		}
 
 	default:
@@ -166,8 +167,8 @@ func verifyRecommendedChair(ctx context.Context, c *client.Client, filePath stri
 		}
 
 		if !cmp.Equal(*expected, *actual, ignoreChairUnexported) {
-			diff := cmp.Diff(*expected, *actual, ignoreChairUnexported)
-			return failure.New(fails.ErrApplication, failure.Message("GET /api/recommended_chair: イスのおすすめ結果が不正です"), failure.Messagef("snapshot: %s", filePath), failure.Message(diff))
+			log.Printf("%s\n%s\n", filePath, cmp.Diff(*expected, *actual, ignoreChairUnexported))
+			return failure.New(fails.ErrApplication, failure.Message("GET /api/recommended_chair: イスのおすすめ結果が不正です"), failure.Messagef("snapshot: %s", filePath))
 		}
 
 	default:
@@ -200,8 +201,8 @@ func verifyRecommendedEstate(ctx context.Context, c *client.Client, filePath str
 		}
 
 		if !cmp.Equal(*expected, *actual, ignoreEstateUnexported) {
-			diff := cmp.Diff(*expected, *actual, ignoreEstateUnexported)
-			return failure.New(fails.ErrApplication, failure.Message("GET /api/recommended_estate: 物件のおすすめ結果が不正です"), failure.Messagef("snapshot: %s", filePath), failure.Message(diff))
+			log.Printf("%s\n%s\n", filePath, cmp.Diff(*expected, *actual, ignoreEstateUnexported))
+			return failure.New(fails.ErrApplication, failure.Message("GET /api/recommended_estate: 物件のおすすめ結果が不正です"), failure.Messagef("snapshot: %s", filePath))
 		}
 
 	default:
@@ -242,8 +243,8 @@ func verifyRecommendedEstateWithChair(ctx context.Context, c *client.Client, fil
 			return failure.Translate(err, fails.ErrBenchmarker, failure.Message("GET /api/recommended_estate:id: Response BodyのUnmarshalでエラーが発生しました"))
 		}
 		if !cmp.Equal(*expected, *actual, ignoreEstateUnexported) {
-			diff := cmp.Diff(*expected, *actual, ignoreEstateUnexported)
-			return failure.New(fails.ErrApplication, failure.Message("GET /api/recommended_estate:id: 物件のおすすめ結果が不正です"), failure.Messagef("snapshot: %s", filePath), failure.Message(diff))
+			log.Printf("%s\n%s\n", filePath, cmp.Diff(*expected, *actual, ignoreEstateUnexported))
+			return failure.New(fails.ErrApplication, failure.Message("GET /api/recommended_estate:id: 物件のおすすめ結果が不正です"), failure.Messagef("snapshot: %s", filePath))
 		}
 
 	default:
@@ -282,8 +283,8 @@ func verifyEstateNazotte(ctx context.Context, c *client.Client, filePath string)
 		}
 
 		if !cmp.Equal(*expected, *actual, ignoreEstateUnexported) {
-			diff := cmp.Diff(*expected, *actual, ignoreEstateUnexported)
-			return failure.New(fails.ErrApplication, failure.Message("POST /api/estate/nazotte: 物件の検索結果が不正です"), failure.Messagef("snapshot: %s", filePath), failure.Message(diff))
+			log.Printf("%s\n%s\n", filePath, cmp.Diff(*expected, *actual, ignoreEstateUnexported))
+			return failure.New(fails.ErrApplication, failure.Message("POST /api/estate/nazotte: 物件の検索結果が不正です"), failure.Messagef("snapshot: %s", filePath))
 		}
 
 	default:
