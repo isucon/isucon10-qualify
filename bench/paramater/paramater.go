891096b3bfedd17b3471130c1f5f5f3048dc31db
Shunsuke Mano <19568747+progfay@users.noreply.github.com>
2020-08-03 11:10:00 +0900

    [bench] POST /initializeのTimeoutを30秒に変更 (#38)

--- bench/paramater/paramater.go
+++ bench/paramater/paramater.go
@@ -22,7 +22,7 @@ const (
 	IntervalForCheckWorkers               = 5 * time.Second
 	ThresholdTimeOfAbandonmentPage        = 1 * time.Second
 	DefaultAPITimeout                     = 2000 * time.Millisecond
-	InitializeTimeout                     = 180 * time.Second
+	InitializeTimeout                     = 30 * time.Second
 	VerifyTimeout                         = 10 * time.Second
 	LoadTimeout                           = 60 * time.Second
 )
