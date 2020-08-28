5f15f212445b5644346e25fbda1e840c88fefe39
Takuya Kosugiyama <re@itkq.jp>
2020-08-28 23:53:20 +0900

    Implement /api/chair/:id

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'mysql2'
+require 'mysql2-cs-bind'
 
 class App < Sinatra::Base
   LIMIT = 20
@@ -35,5 +37,28 @@ class App < Sinatra::Base
     end
 
     { language: 'ruby' }.to_json
+  end
+
+  get '/api/chair/:id' do
+    id =
+      begin
+        Integer(params[:id], 10)
+      rescue ArgumentError => e
+        logger.error "Request parameter \"id\" parse error: #{e.inspect}"
+        halt 400
+      end
+
+    chair = db.xquery('SELECT * FROM chair WHERE id = ?', id).first
+    unless chair
+      logger.info "Requested id's chair not found: #{id}"
+      halt 404
+    end
+
+    if chair['stock'] <= 0
+      logger.info("Requested id's chair is sold out: #{id}")
+      halt 404
+    end
+
+    chair.to_json
   end
 end
