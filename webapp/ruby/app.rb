7fb56629c383989cc70bdbd32380b4093af828f1
Takuya Kosugiyama <re@itkq.jp>
2020-08-29 19:15:17 +0900

    Implement /api/estate/search

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -278,6 +278,103 @@ class App < Sinatra::Base
     { estates: estates.map { |e| capitalize_keys_for_estate(e) } }.to_json
   end
 
+  get '/api/estate/search' do
+    search_queries = []
+    query_params = []
+
+    if params[:doorHeightRangeId] && params[:doorHeightRangeId].size > 0
+      door_height = ESTATE_SEARCH_CONDITION[:doorHeight][:ranges][params[:doorHeightRangeId].to_i]
+      unless door_height
+        logger.error "doorHeightRangeId invalid: #{params[:doorHeightRangeId]}"
+        halt 400
+      end
+
+      if door_height[:min] != -1
+        search_queries << 'door_height >= ?'
+        query_params << door_height[:min]
+      end
+
+      if door_height[:max] != -1
+        search_queries << 'door_height < ?'
+        query_params << door_height[:max]
+      end
+    end
+
+    if params[:doorWidthRangeId] && params[:doorWidthRangeId].size > 0
+      door_width = ESTATE_SEARCH_CONDITION[:doorWidth][:ranges][params[:doorWidthRangeId].to_i]
+      unless door_width
+        logger.error "doorWidthRangeId invalid: #{params[:doorWidthRangeId]}"
+        halt 400
+      end
+
+      if door_width[:min] != -1
+        search_queries << 'door_width >= ?'
+        query_params << door_width[:min]
+      end
+
+      if door_width[:max] != -1
+        search_queries << 'door_width < ?'
+        query_params << door_width[:max]
+      end
+    end
+
+    if params[:rentRangeId] && params[:rentRangeId].size > 0
+      rent = ESTATE_SEARCH_CONDITION[:rent][:ranges][params[:rentRangeId].to_i]
+      unless rent
+        logger.error "rentRangeId invalid: #{params[:rentRangeId]}"
+        halt 400
+      end
+
+      if rent[:min] != -1
+        search_queries << 'rent >= ?'
+        query_params << rent[:min]
+      end
+
+      if rent[:max] != -1
+        search_queries << 'rent < ?'
+        query_params << rent[:max]
+      end
+    end
+
+    if params[:features] && params[:features].size > 0
+      params[:features].split(',').each do |feature_condition|
+        search_queries << "features LIKE CONCAT('%', ?, '%')"
+        query_params.push(feature_condition)
+      end
+    end
+
+    if search_queries.size == 0
+      logger.error "Search condition not found"
+      halt 400
+    end
+
+    page =
+      begin
+        Integer(params[:page], 10)
+      rescue ArgumentError => e
+        logger.error "Invalid format page parameter: #{e.inspect}"
+        halt 400
+      end
+
+    per_page =
+      begin
+        Integer(params[:perPage], 10)
+      rescue ArgumentError => e
+        logger.error "Invalid format perPage parameter: #{e.inspect}"
+        halt 400
+      end
+
+    sqlprefix = 'SELECT * FROM estate WHERE '
+    search_condition = search_queries.join(' AND ')
+    limit_offset = " ORDER BY popularity DESC, id ASC LIMIT #{per_page} OFFSET #{per_page * page}" # XXX:
+    count_prefix = 'SELECT COUNT(*) as count FROM estate WHERE '
+
+    count = db.xquery("#{count_prefix}#{search_condition}", query_params).first['count']
+    estates = db.xquery("#{sqlprefix}#{search_condition}#{limit_offset}", query_params).to_a
+
+    { count: count, estates: estates.map { |e| capitalize_keys_for_estate(e) } }.to_json
+  end
+
   get '/api/estate/:id' do
     id =
       begin
