b2a63be38b97f9098012a0b262f68387ff1c625a
progfay <shun1856shun@gmail.com>
2020-09-02 18:31:22 +0900

    入稿時にエラーが起きたらfailさせる

--- bench/scenario/chairDraftPostScenario.go
+++ bench/scenario/chairDraftPostScenario.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/isucon10-qualify/isucon10-qualify/bench/client"
 	"github.com/isucon10-qualify/isucon10-qualify/bench/fails"
+	"github.com/morikuni/failure"
 )
 
 func chairDraftPostScenario(ctx context.Context, c *client.Client, filePath string) {
 	err := c.PostChairs(ctx, filePath)
 	if err != nil {
-		fails.ErrorsForCheck.Add(err, fails.ErrorOfChairDraftPostScenario)
+		fails.ErrorsForCheck.Add(failure.Translate(err, fails.ErrCritical), fails.ErrorOfChairDraftPostScenario)
 	}
 }
