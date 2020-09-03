3d8b69298fd1705ecaf08820c1443ff75c6ef8fe
Shunsuke Mano <19568747+progfay@users.noreply.github.com>
2020-09-03 15:50:23 +0900

    [bench] Scenario形式のVerifyでCSV入稿APIを検証する (#122)

--- bench/scenario/chairDraftPostScenario.go
+++ bench/scenario/chairDraftPostScenario.go
@@ -2,14 +2,45 @@ package scenario
 
 import (
 	"context"
+	"encoding/json"
+	"io"
+	"os"
 
+	"github.com/isucon10-qualify/isucon10-qualify/bench/asset"
 	"github.com/isucon10-qualify/isucon10-qualify/bench/client"
 	"github.com/isucon10-qualify/isucon10-qualify/bench/fails"
 	"github.com/morikuni/failure"
 )
 
+func loadChairsFromJSON(ctx context.Context, filePath string) ([]asset.Chair, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	chairs := []asset.Chair{}
+	decoder := json.NewDecoder(f)
+	for {
+		if err := ctx.Err(); err != nil {
+			return nil, err
+		}
+		var chair asset.Chair
+		if err := decoder.Decode(&chair); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		chairs = append(chairs, chair)
+	}
+
+	return chairs, nil
+}
+
 func chairDraftPostScenario(ctx context.Context, c *client.Client, filePath string) {
-	err := c.PostChairs(ctx, filePath)
+	chairs, err := loadChairsFromJSON(ctx, filePath)
+	err = c.PostChairs(ctx, chairs)
 	if err != nil {
 		fails.Add(failure.Translate(err, fails.ErrCritical), fails.ErrorOfChairDraftPostScenario)
 	}
--- bench/scenario/estateDraftPostScenario.go
+++ bench/scenario/estateDraftPostScenario.go
@@ -2,14 +2,45 @@ package scenario
 
 import (
 	"context"
+	"encoding/json"
+	"io"
+	"os"
 
+	"github.com/isucon10-qualify/isucon10-qualify/bench/asset"
 	"github.com/isucon10-qualify/isucon10-qualify/bench/client"
 	"github.com/isucon10-qualify/isucon10-qualify/bench/fails"
 	"github.com/morikuni/failure"
 )
 
+func loadEstatesFromJSON(ctx context.Context, filePath string) ([]asset.Estate, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	estates := []asset.Estate{}
+	decoder := json.NewDecoder(f)
+	for {
+		if err := ctx.Err(); err != nil {
+			return nil, err
+		}
+		var estate asset.Estate
+		if err := decoder.Decode(&estate); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		estates = append(estates, estate)
+	}
+
+	return estates, nil
+}
+
 func estateDraftPostScenario(ctx context.Context, c *client.Client, filePath string) {
-	err := c.PostEstates(ctx, filePath)
+	estates, err := loadEstatesFromJSON(ctx, filePath)
+	err = c.PostEstates(ctx, estates)
 	if err != nil {
 		fails.Add(failure.Translate(err, fails.ErrCritical), fails.ErrorOfEstateDraftPostScenario)
 	}
