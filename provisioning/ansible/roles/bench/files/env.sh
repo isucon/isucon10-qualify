75970d8a8272a58e9e101845fd2aaa221569b902
mikiya771 <22876341+mikiya771@users.noreply.github.com>
2020-09-10 23:47:47 +0900

    fix: fix servicefiles

--- provisioning/ansible/roles/bench/files/env.sh
+++ provisioning/ansible/roles/bench/files/env.sh
@@ -1,3 +1,3 @@
 ISUXPORTAL_SUPERVISOR_ENDPOINT_URL=https://portal-grpc-dev.x.isucon.dev:443
 ISUXPORTAL_SUPERVISOR_TOKEN=devtoken
-RUST_LOG=${RUST_LOG:-"info,isuxportal_supervisor=trace"}
+RUST_LOG="info,isuxportal_supervisor=trace"
