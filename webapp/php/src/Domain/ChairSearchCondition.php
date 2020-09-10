1d7cfb082ebdf50f4ac146eebcbdd2b616c46b52
progfay <shun1856shun@gmail.com>
2020-09-11 06:50:00 +0900

    fix

--- webapp/php/src/Domain/ChairSearchCondition.php
+++ webapp/php/src/Domain/ChairSearchCondition.php
@@ -8,18 +8,18 @@ class ChairSearchCondition
     public ?RangeCondition $height;
     public ?RangeCondition $depth;
     public ?RangeCondition $price;
-    public ?RangeCondition $color;
-    public ?RangeCondition $feature;
-    public ?RangeCondition $kind;
+    public ?ListCondition $color;
+    public ?ListCondition $feature;
+    public ?ListCondition $kind;
 
     public function __construct(
         RangeCondition $width = null,
         RangeCondition $height = null,
         RangeCondition $depth = null,
         RangeCondition $price = null,
-        RangeCondition $color = null,
-        RangeCondition $feature = null,
-        RangeCondition $kind = null
+        ListCondition $color = null,
+        ListCondition $feature = null,
+        ListCondition $kind = null
     ) {
         $this->width = $width;
         $this->height = $height;
@@ -37,9 +37,9 @@ class ChairSearchCondition
             isset($json['height']) ? RangeCondition::unmarshal($json['height']) : null,
             isset($json['depth']) ? RangeCondition::unmarshal($json['depth']) : null,
             isset($json['price']) ? RangeCondition::unmarshal($json['price']) : null,
-            isset($json['color']) ? RangeCondition::unmarshal($json['color']) : null,
-            isset($json['features']) ? RangeCondition::unmarshal($json['features']) : null,
-            isset($json['kind']) ? RangeCondition::unmarshal($json['kind']) : null,
+            isset($json['color']) ? ListCondition::unmarshal($json['color']) : null,
+            isset($json['feature']) ? ListCondition::unmarshal($json['feature']) : null,
+            isset($json['kind']) ? ListCondition::unmarshal($json['kind']) : null,
         );
     }
 }
